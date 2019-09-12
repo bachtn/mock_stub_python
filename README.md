5e1667a50bcd92cb98f43cae8ebd7c26d36afba2
Alaa Bakhti <alaa.bakhti1993@gmail.com>
2019-09-13 00:50:57 +0200

    use pytest fixtures to share ressources

--- README.md
+++ README.md
@@ -1,23 +1,16 @@
 pytest -vv -p no:warnings
 
-
-# TODO
+# Fake It Before You Make It
 * Object qu'on peut controler
 
 ## Mock
 * Un objet de substitut dans le système, qui décide si un test unitaire passe ou échoue.
-- Test face_recognition calls GPU function (batch_face_locations)
 
 ## Stub
 * On stub une dépendance (fonction, API, Route, …) => Je m’intéresse à ce que cette dépendance retourne
-- 
+
 
 ## Fixture
 * Partager des ressources entre différentes fonctions
 - Loads all the images only one time
-
-
-
-
-# Senario
-
+- # 4.29s -> 2.45s
--- test_image_tools.py
+++ test_image_tools.py
@@ -5,11 +5,18 @@ import pytest
 from image_tools import load_images, save_images, convert_to_jpeg, save_binary_file
 
 
+@pytest.fixture()
+def image_list_and_names():
+    image_list, image_name_list = load_images('images')
+    return image_list, image_name_list
+
+
 @patch('image_tools.save_binary_file')
 @patch('image_tools.cv2.imwrite')
-def test_save_images_should_not_call_cv2_imwrite_function(imwrite_mock, save_binary_file_mock):
+def test_save_images_should_not_call_cv2_imwrite_function(imwrite_mock, save_binary_file_mock,
+                                                          image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
 
     # When
     save_images(image_list, image_name_list, 'images')
@@ -19,9 +26,9 @@ def test_save_images_should_not_call_cv2_imwrite_function(imwrite_mock, save_bin
 
 
 @patch('image_tools.cv2.imencode')
-def test_convert_to_jpeg_calls_cv2_imencode_method(imencode_mock):
+def test_convert_to_jpeg_calls_cv2_imencode_method(imencode_mock, image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
     target_image = image_list[0]
     imencode_mock.return_value = (True, target_image)
 
@@ -33,9 +40,9 @@ def test_convert_to_jpeg_calls_cv2_imencode_method(imencode_mock):
 
 
 @patch('image_tools.cv2.imencode')
-def test_convert_to_jpeg_raises_an_exception_if_the_conversion_fails(imencode_mock):
+def test_convert_to_jpeg_raises_an_exception_if_the_conversion_fails(imencode_mock, image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
     target_image = image_list[0]
     imencode_mock.return_value = (False, target_image)
 
@@ -47,9 +54,10 @@ def test_convert_to_jpeg_raises_an_exception_if_the_conversion_fails(imencode_mo
 
 @patch('image_tools.save_binary_file')
 @patch('image_tools.convert_to_jpeg')
-def test_save_images_should_call_convert_to_jpeg_function(convert_to_jpeg_mock, save_binary_file_mock):
+def test_save_images_should_call_convert_to_jpeg_function(convert_to_jpeg_mock, save_binary_file_mock,
+                                                          image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
 
     # When
     save_images(image_list, image_name_list, 'images')
@@ -59,9 +67,9 @@ def test_save_images_should_call_convert_to_jpeg_function(convert_to_jpeg_mock, 
 
 
 @patch('builtins.open')
-def test_save_binary_path_uses_open_builtin_to_save_images(open_builtin_mock):
+def test_save_binary_path_uses_open_builtin_to_save_images(open_builtin_mock, image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
 
     # When
     save_binary_file('fake_path', image_list[0])
@@ -72,9 +80,9 @@ def test_save_binary_path_uses_open_builtin_to_save_images(open_builtin_mock):
 
 @patch('image_tools.convert_to_jpeg')
 @patch('image_tools.save_binary_file')
-def test_save_images_calls_save_binary_file_function(save_binary_file_mock, convert_to_jpeg_mock):
+def test_save_images_calls_save_binary_file_function(save_binary_file_mock, convert_to_jpeg_mock, image_list_and_names):
     # Given
-    image_list, image_name_list = load_images('images')
+    image_list, image_name_list = image_list_and_names
 
     # When
     save_images(image_list, image_name_list, 'images')
